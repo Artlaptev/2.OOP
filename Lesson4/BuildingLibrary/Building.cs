3a0a70555024296ef5272b5401ac3ff57de880c9
temkalap <89065927+temkalap@users.noreply.github.com>
2022-07-13 11:25:25 +0300

    AddFiles

---
+++ Lesson4/BuildingLibrary/Building.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingLibraryDLL
+{
+    public class Building
+    {
+        public static int lastCreatedBuilding;
+
+
+        private string _id;
+        private int _qtyFloor;
+        private int _qtySection;
+        private int _qtyFlatsOnTheFloor=3;
+        private int _heightOfFloor = 2700;
+        
+        
+        public Building(int qtyFloor,int qtysection)
+        {
+
+            SetID();
+            _qtyFloor= qtyFloor;
+            _qtySection= qtysection;
+        }
+        private void SetID()
+        {
+            lastCreatedBuilding++;
+            _id = lastCreatedBuilding.ToString().PadLeft(4,'0');
+        }
+        private void SetQtyFloor(int qty)
+        {
+            if(qty>0)
+                _qtyFloor = qty;
+            else
+                throw new Exception("Колличество должно быть положительным");
+        }
+        private void SetQtySection(int qty)
+        {
+            if (qty > 0)
+                _qtySection = qty;
+            else
+                throw new Exception("Колличество должно быть положительным");
+        }
+        public int GetQtyFlatsInBiulding()
+        {
+            return  GetQtyFlatsInSection()* _qtySection;
+        }
+        public int GetQtyFlatsInSection()
+        {
+            return _qtyFloor * _qtyFlatsOnTheFloor;
+        }
+        public int GetHeight()
+        {
+            return 2700 * _qtyFloor + 1900 + 1200;
+        }
+
+        public string GetId()
+        {
+            return _id;
+        }
+
+        public override string ToString()
+        {
+            return "Здание: "+_id + ", Количество этажей: "+_qtyFloor+", Количество подъездов: " +_qtySection+", Всего квартир: "+GetQtyFlatsInBiulding()+", Высота: "+GetHeight();
+        }
+    }
+   
+}
---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,9 @@
+﻿/*using BuildingLibrary;*/ //Проект в текущем решении
+using BuildingLibraryDLL;//библиотека dll
+
+Console.WriteLine(Creator.CreateBuild(9, 2));
+Console.WriteLine(Creator.CreateBuild(22, 10));
+Console.WriteLine(Creator.CreateBuild(3, 2));
+Console.WriteLine(Creator.CreateBuild(5, 2));
+Console.WriteLine(Creator.CreateBuild(9, 3));
+Console.ReadKey();
