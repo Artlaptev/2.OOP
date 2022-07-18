c5ac36a6c7adaf5f90bd6cd4eed89d011df01ee6
temkalap <89065927+temkalap@users.noreply.github.com>
2022-07-18 16:20:54 +0300

    create property and constructor Task2

---
+++ Lesson5/Lesson5/Lesson5/Rational.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Rational
+    {
+        int _numerator;
+        int _denominator;
+        private string Fraction
+        {
+            get { return this.ToString(); }
+            set { _numerator = int.Parse(value.Split('/')[0]); }
+        }
+
+
+    }
+}
---
+++ Lesson5/Lesson5/Task2/Complex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Complex
+    {
+        int Real;
+        int Imaginary;
+
+        private string  PropComplex
+        {
+
+            set
+            {
+                value = value.Trim('i');
+                string [] elements = value.Split('+');
+                Real = int.Parse(elements[0]);
+                Imaginary = int.Parse(elements[1]);
+            }
+            
+        }
+        public Complex(string complexNumber)
+        {
+            PropComplex = complexNumber;
+        }
+        public override string ToString()
+        {
+            return $"{Real}+{Imaginary}i";
+        }
+    }
+}
