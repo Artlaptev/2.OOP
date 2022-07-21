b97c31ae37dee0643435eb0723129b41bdd0a964
temkalap <89065927+temkalap@users.noreply.github.com>
2022-07-21 20:51:37 +0300

    Добавлен проект, выполнил 1 задание, начал второе

---
+++ GetAllLevels/GetAllLevels/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSM = Tekla.Structures.Model;
+using TSMUI = Tekla.Structures.Model.UI;
+
+namespace GetAllLevels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TSM.Model model = new TSM.Model();
+            TSM.ModelObjectEnumerator enumer = new TSMUI.ModelObjectSelector().GetSelectedObjects();
+            int level =0;
+            while(enumer.MoveNext())
+            {
+                TSM.Assembly assembly = enumer.Current as TSM.Assembly;
+                
+                GetAssemblyPosAndLevel(assembly, ref level);
+                
+            }
+            Console.WriteLine($"готово");
+            
+            Console.ReadKey();
+        }
+        public static void GetAssemblyPosAndLevel(TSM.Assembly assembly,ref int level)
+        {
+            string assPos = "";
+            assembly.GetReportProperty("ASSEMBLY_POS", ref assPos);
+            Console.WriteLine($"{level} {assPos}");
+            ArrayList array = assembly.GetSubAssemblies();
+            if(array.Count>0)level++;
+            foreach (TSM.Assembly sub in array)
+            {
+                GetAssemblyPosAndLevel(sub,ref level) ;
+            }
+
+            
+        }
+
+    }
+}
