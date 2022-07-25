01deaf1510290939668788d3262f44c5461bb0f9
temkalap <89065927+temkalap@users.noreply.github.com>
2022-07-25 11:18:54 +0300

    Добавлены файлы

---
+++ Lesson7/Task2/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Task2
+{
+    public class Circle:Point
+    {
+        private int r;
+        public int R {
+            get
+            {
+                return r;
+            }
+            private set
+            {
+                r = GetValidSize(value);
+            }
+        }
+
+        public Circle(int size,Color color, BasePoint basePoint )
+        {
+            R = size;
+            Color= color;
+            BasePoint= basePoint;
+        }
+        public double GetArea()
+        {
+            return Math.PI * R *R;
+        }
+    }
+}
