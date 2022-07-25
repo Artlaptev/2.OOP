2d3c8442a1f4d969b8ee6fe545378a75c4e719cf
temkalap <89065927+temkalap@users.noreply.github.com>
2022-07-25 10:22:27 +0300

    Закончено задание 1

---
+++ Lesson7/Lesson7/ACoder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Encodings;
+
+namespace Task1
+{
+    public class ACoder: ICoder
+    {
+        private static List<char> Codelist = Enumerable.Range(' ', 'я' - ' ' + 1).Select(c => (char)c).ToList();
+
+        public string Encode(string str)
+        {
+            string result = string.Empty;
+            foreach(char sym in str)
+            {
+                result += Codelist[Codelist.IndexOf(sym) + 1];
+            }
+            return result;
+        }
+        public string Decode(string str)
+        {
+            string result = string.Empty;
+            foreach (char sym in str)
+            {
+                result += Codelist[Codelist.IndexOf(sym) - 1];
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Lesson7/Lesson7/Program.cs
@@ -0,0 +1,10 @@
+﻿using Task1;
+
+ACoder Acoder=new ACoder();
+BCoder BCoder=new BCoder();
+string enc= Acoder.Encode("Привет Медвед");
+string dec = Acoder.Decode(enc);
+
+string enc1=BCoder.Encode("Привет Медвед");
+string dec1=BCoder.Decode(enc1);
+Console.ReadKey();
