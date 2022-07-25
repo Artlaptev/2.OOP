a4b4ed9a1fe4effde29e3a91c5e1ad6e70a16a82
temkalap <89065927+temkalap@users.noreply.github.com>
2022-07-25 20:42:38 +0300

    Create Interface1.cs

---
+++ FileManager/FileManager/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    public interface IItem
+    {
+        public void Creat();
+        public void Delete();
+        public void Rename();
+        public void Move();
+        public void Copy();
+        public void Paste();
+    }
+}
