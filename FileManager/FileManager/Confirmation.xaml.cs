5b8dd210d8275f717219a47f39e49c482cb7e243
temkalap <89065927+temkalap@users.noreply.github.com>
2022-07-26 16:51:32 +0300

    Добавлены формы

--- FileManager/FileManager/Confirmation.xaml.cs
+++ FileManager/FileManager/Confirmation.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FileManager
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Confirmation.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Confirmation : Window
     {
-        public MainWindow()
+        public Confirmation()
         {
             InitializeComponent();
         }
-        
     }
 }
